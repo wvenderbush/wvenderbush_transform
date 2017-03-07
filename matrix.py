1facac1e5f03a044ca8d459260aa4d1d4642932a
Winston Venderbush <winstonvenderbush@gmail.com>
2017-03-07 11:22:31 -0500

    finished

--- matrix.py
+++ matrix.py
@@ -1,61 +1,91 @@
 import math
 
 def make_translate( x, y, z ):
-    pass
+	mat = new_matrix()
+	ident(mat)
+	mat[3] = [x, y, z, 1]
+	return mat
 
 def make_scale( x, y, z ):
-    pass
+	mat = new_matrix()
+	ident(mat)
+	mat[0][0] = x
+	mat[1][1] = y
+	mat[2][2] = z
+	return mat
 
-def make_rotX( theta ):    
-    pass
+def make_rotX( theta ):
+	radians = theta * (math.pi / 180)    
+	mat = new_matrix()
+	ident(mat)
+	mat[1][1] = math.cos(radians)
+	mat[1][2] = math.sin(radians) * -1
+	mat[2][1] = math.sin(radians)
+	mat[2][2] = math.cos(radians)
+	return mat
 
 def make_rotY( theta ):
-    pass
+	radians = theta * (math.pi / 180)    
+	mat = new_matrix()
+	ident(mat)
+	mat[0][0] = math.cos(radians)
+	mat[0][2] = math.sin(radians)
+	mat[2][0] = math.sin(radians) * -1
+	mat[2][2] = math.cos(radians)
+	return mat
 
 def make_rotZ( theta ):
-    pass
+	radians = theta * (math.pi / 180)    
+	mat = new_matrix()
+	ident(mat)
+	mat[0][0] = math.cos(radians)
+	mat[0][1] = math.sin(radians) * -1
+	mat[1][0] = math.sin(radians)
+	mat[1][1] = math.cos(radians)
+	return mat
 
 def print_matrix( matrix ):
-    s = ''
-    for r in range( len( matrix[0] ) ):
-        for c in range( len(matrix) ):
-            s+= str(matrix[c][r]) + ' '
-        s+= '\n'
-    print s
+	s = ''
+	for r in range( len( matrix[0] ) ):
+		for c in range( len(matrix) ):
+			s+= str(matrix[c][r]) + ' '
+		s+= '\n'
+	print s
 
 def ident( matrix ):
-    for r in range( len( matrix[0] ) ):
-        for c in range( len(matrix) ):
-            if r == c:
-                matrix[c][r] = 1
-            else:
-                matrix[c][r] = 0
+	for r in range( len( matrix[0] ) ):
+		for c in range( len(matrix) ):
+			if r == c:
+				matrix[c][r] = 1
+			else:
+				matrix[c][r] = 0
 
 def scalar_mult( matrix, s ):
-    for r in range( len( matrix[0] ) ):
-        for c in range( len(matrix) ):
-            matrix[c][r]*= s
-            
+	for r in range( len( matrix[0] ) ):
+		for c in range( len(matrix) ):
+			matrix[c][r]*= s
+			
 #m1 * m2 -> m2
 def matrix_mult( m1, m2 ):
 
-    point = 0
-    for row in m2:
-        #get a copy of the next point
-        tmp = row[:]
-        
-        for r in range(4):
-            m2[point][r] = (m1[0][r] * tmp[0] +
-                            m1[1][r] * tmp[1] +
-                            m1[2][r] * tmp[2] +
-                            m1[3][r] * tmp[3])
-        point+= 1
+	point = 0
+	for row in m2:
+		#get a copy of the next point
+		tmp = row[:]
+		
+		for r in range(4):
+			m2[point][r] = (m1[0][r] * tmp[0] +
+							m1[1][r] * tmp[1] +
+							m1[2][r] * tmp[2] +
+							m1[3][r] * tmp[3])
+		point+= 1
 
 
 def new_matrix(rows = 4, cols = 4):
-    m = []
-    for c in range( cols ):
-        m.append( [] )
-        for r in range( rows ):
-            m[c].append( 0 )
-    return m
+	m = []
+	for c in range( cols ):
+		m.append( [] )
+		for r in range( rows ):
+			m[c].append( 0 )
+	return m
+
--- parser.py
+++ parser.py
@@ -1,41 +1,112 @@
 from display import *
 from matrix import *
 from draw import *
+from time import sleep
 
 """
 Goes through the file named filename and performs all of the actions listed in that file.
 The file follows the following format:
-     Every command is a single character that takes up a line
-     Any command that requires arguments must have those arguments in the second line.
-     The commands are as follows:
-         line: add a line to the edge matrix - 
-	    takes 6 arguemnts (x0, y0, z0, x1, y1, z1)
+	 Every command is a single character that takes up a line
+	 Any command that requires arguments must have those arguments in the second line.
+	 The commands are as follows:
+		 line: add a line to the edge matrix - 
+		takes 6 arguemnts (x0, y0, z0, x1, y1, z1)
 	 ident: set the transform matrix to the identity matrix - 
 	 scale: create a scale matrix, 
-	    then multiply the transform matrix by the scale matrix - 
-	    takes 3 arguments (sx, sy, sz)
+		then multiply the transform matrix by the scale matrix - 
+		takes 3 arguments (sx, sy, sz)
 	 translate: create a translation matrix, 
-	    then multiply the transform matrix by the translation matrix - 
-	    takes 3 arguments (tx, ty, tz)
+		then multiply the transform matrix by the translation matrix - 
+		takes 3 arguments (tx, ty, tz)
 	 rotate: create a rotation matrix,
-	    then multiply the transform matrix by the rotation matrix -
-	    takes 2 arguments (axis, theta) axis should be x, y or z
+		then multiply the transform matrix by the rotation matrix -
+		takes 2 arguments (axis, theta) axis should be x, y or z
 	 yrotate: create an y-axis rotation matrix,
-	    then multiply the transform matrix by the rotation matrix -
-	    takes 1 argument (theta)
+		then multiply the transform matrix by the rotation matrix -
+		takes 1 argument (theta)
 	 zrotate: create an z-axis rotation matrix,
-	    then multiply the transform matrix by the rotation matrix -
-	    takes 1 argument (theta)
+		then multiply the transform matrix by the rotation matrix -
+		takes 1 argument (theta)
 	 apply: apply the current transformation matrix to the 
-	    edge matrix
+		edge matrix
 	 display: draw the lines of the edge matrix to the screen
-	    display the screen
+		display the screen
 	 save: draw the lines of the edge matrix to the screen
-	    save the screen to a file -
-	    takes 1 argument (file name)
+		save the screen to a file -
+		takes 1 argument (file name)
 	 quit: end parsing
 
 See the file script for an example of the file format
 """
 def parse_file( fname, points, transform, screen, color ):
-    pass
+	with open(fname) as f:
+		content = f.readlines()
+		content = [x.strip() for x in content]
+		
+	count = 0;
+	plen = len(content)
+
+	while (count < plen):
+		cmd = content[count]
+		arg = content[count + 1]
+
+		if (cmd == "line"):
+			print("Adding line...\n")
+			args = arg.split(" ")
+			add_edge(points, int(args[0]), int(args[1]), int(args[2]), int(args[3]), int(args[4]), int(args[5]))
+			count += 2
+
+		if (cmd == "ident"):
+			print("Reverting to identity...\n")
+			ident(transform)
+			count += 1
+
+		if (cmd == "scale"):
+			print("Scaling matrix...\n")
+			args = arg.split(" ")
+			newmat = make_scale(int(args[0]), int(args[1]), int(args[2]))
+			matrix_mult(newmat, transform)
+			count += 2
+
+		if (cmd == "move"):
+			print("Moving matrix...\n")
+			args = arg.split(" ")
+			newmat = make_translate(int(args[0]), int(args[1]), int(args[2]))
+			matrix_mult(newmat, transform)
+			count += 2
+
+		if (cmd == "rotate"):
+			args = arg.split(" ")
+			if (args[0] == "x"):
+				print("Rotating by x...\n")
+				newmat = make_rotX(int(args[1]))
+			if (args[0] == "y"):
+				print("Rotating by y...\n")
+				newmat = make_rotY(int(args[1]))
+			if (args[0] == "z"):
+				print("Rotating by z...\n")
+				newmat = make_rotZ(int(args[1]))
+			
+			matrix_mult(newmat, transform)
+			count += 2
+
+		if (cmd == "apply"):
+			print("Applying transformation...\n")
+			matrix_mult(transform, points)
+			count += 1
+
+		if (cmd == "display"):
+			print("Starting display...\n")
+			draw_lines(points, screen, color)
+			display(screen)
+			sleep(.25)
+			clear_screen(screen)
+			count += 1
+
+		if (cmd == "save"):
+			print("Saving file...\n")
+			save_extension(screen, arg)
+			#clear_screen(screen)
+			count += 2
+
+		print_matrix(points)
